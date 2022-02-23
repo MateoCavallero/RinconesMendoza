d1a36cccd9f316ee0ec04f4e83fe2f2d829c5ffb
NicolasCastroPino <nicolas.cas.pin@gmail.com>
2022-02-23 20:05:09 -0300

    nico

--- src/main/java/com/example/RinconesMendoza/entidades/Restaurant.java
+++ src/main/java/com/example/RinconesMendoza/entidades/Restaurant.java
@@ -2,11 +2,14 @@ package com.example.RinconesMendoza.entidades;
 
 import com.example.RinconesMendoza.utils.TipoComida;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import lombok.Data;
 
 @Entity
 @Data
 public class Restaurant extends Locacion{
+    @Enumerated(value = EnumType.STRING)
     private TipoComida tipoDeComida;
     private Boolean menuCeliaco;
 }
