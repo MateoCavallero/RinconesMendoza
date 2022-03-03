d3c1f82c21680b46a8484732b286b4f9d4e49aa1
NicolasCastroPino <nicolas.cas.pin@gmail.com>
2022-03-03 19:02:54 -0300

    Comentario

--- src/main/java/com/example/RinconesMendoza/entidades/Locacion.java
+++ src/main/java/com/example/RinconesMendoza/entidades/Locacion.java
@@ -9,7 +9,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
