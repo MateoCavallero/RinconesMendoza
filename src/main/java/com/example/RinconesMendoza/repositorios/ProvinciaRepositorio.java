32a7340f93d30cd7be020d161947141de8e8690d
MateoCavallero <matucavallero123@gmail.com>
2022-03-03 21:22:44 -0300

    commit tmp zonas

---
+++ src/main/java/com/example/RinconesMendoza/repositorios/ProvinciaRepositorio.java
@@ -0,0 +1,8 @@
+package com.example.RinconesMendoza.repositorios;
+
+import com.example.RinconesMendoza.entidades.Provincia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProvinciaRepositorio extends JpaRepository<Provincia, String>{
+    
+}
