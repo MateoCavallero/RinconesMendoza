673568f2ffc598f3b60cd59dbb2c3394907e4cae
Francisco Perez Mota <fperezmota@gmail.com>
2022-03-17 20:13:49 -0300

    correcciones

--- src/main/java/com/example/RinconesMendoza/controladores/MailControlador.java
+++ src/main/java/com/example/RinconesMendoza/controladores/MailControlador.java
@@ -1,12 +1,7 @@
 package com.example.RinconesMendoza.controladores;
 
 import com.example.RinconesMendoza.servicios.MailServicio;
-import java.io.File;
-import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
