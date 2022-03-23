5782472c14ca3e327c803db448c85a2082618c8f
Caro Cotani <carocotani@gmail.com>
2022-03-22 21:32:43 -0300

    controladores zonas

--- src/main/java/com/example/RinconesMendoza/repositorios/AlojamientoRepositorio.java
+++ src/main/java/com/example/RinconesMendoza/repositorios/AlojamientoRepositorio.java
@@ -10,19 +10,19 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AlojamientoRepositorio extends JpaRepository<Alojamiento, String> {
 
-    @Query("SELECT l FROM Alojamiento l WHERE l.zona.id = '17193838-a433-4b46-b977-9535e71a7dad'")
+    @Query("SELECT l FROM Alojamiento l WHERE l.zona.id = '6796dc3c-70fe-4726-b289-bd40a8672422'")
     List<Alojamiento> findAllUco();
 
-    @Query("SELECT l FROM Alojamiento l WHERE l.zona.id = '4ab268bc-9522-490c-b08c-589ec137b327'")
+    @Query("SELECT l FROM Alojamiento l WHERE l.zona.id = '5621682c-a80f-4ec8-a3e7-04b80bca4e35'")
     List<Alojamiento> findAllGranMendoza();
 
-    @Query("SELECT l FROM Alojamiento l WHERE l.zona.id = '156c9d3f-5600-4631-84a4-b2d59f696456'")
+    @Query("SELECT l FROM Alojamiento l WHERE l.zona.id = '5496c668-1f96-40a9-927b-b1dc9c279da2'")
     List<Alojamiento> findAllZonaSur();
 
-    @Query("SELECT l FROM Alojamiento l WHERE l.zona.id = '69256f49-6d8f-4fdc-8afa-7166e194b545'")
+    @Query("SELECT l FROM Alojamiento l WHERE l.zona.id = '728c9365-980a-4ab7-b854-fec06efb1be6'")
     List<Alojamiento> findAllZonaEste();
 
-    @Query("SELECT l FROM Alojamiento l WHERE l.zona.id = '66dc0dbc-bf96-4261-8442-f6b8ff468136'")
+    @Query("SELECT l FROM Alojamiento l WHERE l.zona.id = '63a3a764-4e26-4d08-8f92-58e34f0092cc'")
     List<Alojamiento> findAllAltaMontana();
 
     @Query("SELECT l FROM Alojamiento l WHERE l.nombre LIKE :q")
